4a8803c36943bbc861d0e77ce54241283321036b
Lap0chka <danya.tkachenko.1997@gmail.com>
2024-06-30 15:13:07 +0200

    add admin 30.06.2024

--- shopexample/shop/admin.py
+++ shopexample/shop/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from shop.models import Category, Product
 
-# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description']
+    ordering = ['title']
+    list_filter = ['available', 'create_at']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    ordering = ['name']
--- shopexample/shop/models.py
+++ shopexample/shop/models.py
@@ -1,29 +1,72 @@
 import random
+from string import ascii_letters, digits
 
 from django.db import models
 from django.utils.text import slugify
-from string import ascii_letters, digits
 
 
-def rand_slug():
-    return "".join([random.choice(ascii_letters+digits) for _ in range(3)])
+def rand_slug() -> str:
+    """
+    Generate a random slug consisting of 3 alphanumeric characters.
+    """
+    return "".join([random.choice(ascii_letters + digits) for _ in range(3)])
 
 
-class Categories(models.Model):
+class Category(models.Model):
+    """
+    Represents a category for organizing products.
+    """
     name = models.CharField(max_length=124, db_index=True)
     parent = models.ForeignKey(
-        'self', on_delete=models.CASCADE, null=True, blank=True
+        'self', on_delete=models.CASCADE, null=True, blank=True, related_name='children'
     )
     slug = models.SlugField(max_length=140, unique=True, null=False)
     create_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         unique_together = (['slug', 'parent'])
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name
 
     def save(self, *args, **kwargs):
+        """
+        Save the category instance to the database.
+        """
         if not self.slug:
             self.slug = slugify(rand_slug() + self.name)
-        super().save()
+        super().save(*args, **kwargs)
+
+
+class ProductManage(models.Manager):
+    """
+    Custom manager for the Product model to filter available products.
+    """
+    def get_queryset(self):
+        """
+        Return a queryset of available products.
+        """
+        return super().get_queryset().filter(available=True)
+
+
+class Product(models.Model):
+    """
+    Represents a product in the catalog.
+    """
+    title = models.CharField(max_length=248, db_index=True)
+    slug = models.SlugField(max_length=264, unique=True)
+    brand = models.CharField(max_length=248)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=7, decimal_places=2, default=99.99)
+    image = models.ImageField(upload_to='products/products/%Y/%m/%d')
+    available = models.BooleanField(default=False)
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+
+    availability = ProductManage()
+    objects = models.Manager()
+
+    def __str__(self):
+        return self.title
